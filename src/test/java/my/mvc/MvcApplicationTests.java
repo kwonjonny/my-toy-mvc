c5350cef71faa2e750f37fdd969e229441ea5fed
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-06-09 14:30:11 +0900

    spring boot 설정과 ConnectionTest
    spring boot 설정과 ConnectionTest

--- src/test/java/my/mvc/MvcApplicationTests.java
+++ src/test/java/my/mvc/MvcApplicationTests.java
@@ -1,13 +1,37 @@
 package my.mvc;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Log4j2
 @SpringBootTest
 class MvcApplicationTests {
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 	}
+
+	// ConnectionTest
+	@Test
+	public void connectionTest() {
+		try(Connection connection = dataSource.getConnection()) {
+			log.info("==============================");
+			log.info("==============================");
+			log.info("connection" + connection);
+			log.info("==============================");
+			log.info("==============================");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 }
